676956b9c8083fbe11f996a09f60821e665c4818
GorbunovaAnna <anyutachke@gmail.com>
2023-05-08 17:23:20 +0300

    feat: copy files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+const fs = require("fs");
+const fsPromises = fs.promises;
+const path = require("path");
+const folderPath = path.join(__dirname, "files");
 
+console.log(path.resolve(__dirname));
+fsPromises
+  .readdir(path.join(__dirname), {
+    withFileTypes: true,
+  })
+  .then((dirs) => {
+    const isFound = dirs.find((dir) => {
+      return dir.isDirectory() && dir.name === "copy-files";
+    });
+    if (!isFound) {
+      fsPromises.mkdir(path.join(__dirname, "copy-files"));
+    }
+  })
+  // join into files
+  .then(() => {
+    fsPromises
+      .readdir(folderPath, {
+        withFileTypes: true,
+      })
+      .then((res) => {
+        res.forEach((el) => {
+          const filePath = path.join(folderPath, el.name);
+          fsPromises.readFile(filePath).then((file) => {
+            fsPromises.writeFile(
+              path.join(__dirname, "copy-files", el.name),
+              file
+            );
+          });
+        });
+      });
+  });
